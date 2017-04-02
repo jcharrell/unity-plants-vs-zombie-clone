acf85a3e80212bdf277c515366a63c5a8b24be12
Chris Harrell <chris.harrell@sq1.com>
2017-04-01 21:45:55 -0500

    Remove need for splash screen script to load start menu on app load

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,6 +2,12 @@
 using System.Collections;
 
 public class LevelManager : MonoBehaviour {
+	public float autoLoadNextLevelAfter;
+
+	void Start() {
+		Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
 	public void LoadLevel(string name) {		
 		Application.LoadLevel (name);
 	}
