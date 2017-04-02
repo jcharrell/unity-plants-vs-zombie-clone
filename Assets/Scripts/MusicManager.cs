2d4daa61982e11f8b3d97ad59003a27684c6a4bc
Chris Harrell <chris.harrell@sq1.com>
2017-04-02 13:49:56 -0500

    Create music manager and have it load music for each level

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArr;
+	private AudioSource audioSource;
+
+	void Start() {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void Awake() {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void OnLevelWasLoaded(int level) {
+		AudioClip levelMusic = levelMusicChangeArr[level];
+
+		if(levelMusic) {
+			audioSource.clip = levelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
