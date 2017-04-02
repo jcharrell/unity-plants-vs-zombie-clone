15c61cea1131407a7e7c1a6dd52472b98817adbc
Chris Harrell <chris.harrell@sq1.com>
2017-04-01 21:24:07 -0500

    Added basic splash screen and start menu

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadStartScreen", 2f);
+	}
+
+	void LoadStartScreen() {
+		LevelManager levelManager = GameObject.Find("Level Manager").GetComponent<LevelManager>();
+		levelManager.LoadNextLevel();
+	}
+
+}
