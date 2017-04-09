6a4cae491a66ca47e79401dc7e8d191a8ecc36de
Chris Harrell <chris.harrell@sq1.com>
2017-04-09 09:32:47 -0500

    Only auto load if setting is NOT set to 0

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,7 +5,7 @@ public class LevelManager : MonoBehaviour {
 	public float autoLoadNextLevelAfter = 0;
 
 	void Start() {
-		if(autoLoadNextLevelAfter == 0) {
+		if(autoLoadNextLevelAfter != 0) {
 			Invoke("LoadNextLevel", autoLoadNextLevelAfter);
 		}
 	}
